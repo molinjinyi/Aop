8a976a0c41838958bdd406a51263669214dcad4b
Ada <584507257@qq.com>
2020-10-04 15:40:42 +0800

    约定大于配置:容器挂载磁盘的时候为每个服务的日志命名一个文件夹，来达到区分服务日志的约定，同时用容器id作为同一个服务不同的机器生成不同的文件，方便追溯
    统一的日志格式，防止组内的开发人员随意的引入日志
    按日志级别进行分文件记录
    同时在后期根据需要升级日志DLL文件
    分为api和console日志记录

---
+++ src/Logging/Ada.Aop.Logging.Web/Microsoft/Extensions/Hosting/HostingHostBuilderExtensions.cs
@@ -0,0 +1,16 @@
+﻿using NLog.Web;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.Extensions.Hosting
+{
+    public static class HostingHostBuilderExtensions
+    {
+        public static IHostBuilder UseNLog(this IHostBuilder hostBuilder)
+        {
+            hostBuilder.UseNLog();
+            return hostBuilder;
+        }
+    }
+}
---
+++ src/Logging/Samples/Ada.SampleA.BackgroundTasks/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ada.SampleA.BackgroundTasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
