ff9209118b42d658de7a4f0515a3b2733e764e1e
CardAppH006 <CardAppH006@CARDAPPH006-PC>
2020-11-05 20:21:54 +0800

    AsyncLocal學習測試

--- test/AsyncLocal.Test/Program.cs
+++ test/AsyncLocal.Test/Program.cs
@@ -21,7 +21,6 @@ namespace AsyncLocal.Test
             _asyncLocalString.Value = "Value 2";
             _threadLocalString.Value = "Value 2";
             var t2 = AsyncMethodB("Value 2");
-
             // Await both calls
             await t1;
             await t2;
@@ -30,17 +29,41 @@ namespace AsyncLocal.Test
         static async Task AsyncMethodB(string expectedValue)
         {
             Console.WriteLine("Entering AsyncMethodB.");
-            Console.WriteLine("   Expected '{0}', AsyncLocal value is '{1}', ThreadLocal value is '{2}'",
-                              expectedValue, _asyncLocalString.Value, _threadLocalString.Value);
+            Console.WriteLine("   Expected '{0}', AsyncLocal value is '{1}', ThreadLocal value is '{2}',CurrentCulture:{3},CurrentUICulture:{4},ManagedThreadId:{5},IsBackground:{6},IsThreadPoolThread:{7}",
+                              expectedValue, _asyncLocalString.Value, _threadLocalString.Value,Thread.CurrentThread.CurrentCulture,
+                              Thread.CurrentThread.CurrentUICulture, Thread.CurrentThread.ManagedThreadId, Thread.CurrentThread.IsBackground, Thread.CurrentThread.IsThreadPoolThread);
             await Task.Delay(100);
             Console.WriteLine("Exiting AsyncMethodB.");
-            Console.WriteLine("   Expected '{0}', got '{1}', ThreadLocal value is '{2}'",
-                              expectedValue, _asyncLocalString.Value, _threadLocalString.Value);
+            Console.WriteLine("   Expected '{0}', got '{1}', ThreadLocal value is '{2}',CurrentCulture:{3},CurrentUICulture:{4},ManagedThreadId:{5},IsBackground:{6},IsThreadPoolThread:{7}",
+                              expectedValue, _asyncLocalString.Value, _threadLocalString.Value, Thread.CurrentThread.CurrentCulture,
+                              Thread.CurrentThread.CurrentUICulture, Thread.CurrentThread.ManagedThreadId, Thread.CurrentThread.IsBackground, Thread.CurrentThread.IsThreadPoolThread);
         }
 
         static async Task Main(string[] args)
         {
-            await AsyncMethodA();
+            //await AsyncMethodA();
+            Console.WriteLine("   CurrentCulture:{0},CurrentUICulture:{1},ManagedThreadId:{2},IsBackground:{3},IsThreadPoolThread:{4}",
+                               Thread.CurrentThread.CurrentCulture,
+                              Thread.CurrentThread.CurrentUICulture, Thread.CurrentThread.ManagedThreadId, Thread.CurrentThread.IsBackground, Thread.CurrentThread.IsThreadPoolThread);
+            for (int i = 0; i < 10; i++)
+            {
+                await Task.Run(() =>
+                 {
+                     TestAsyncLocal.Instance.SetAsyncLocalString($"第{i}個");
+                     Console.WriteLine("   Expected '{0}', got '{1}', ThreadLocal value is '{2}',CurrentCulture:{3},CurrentUICulture:{4},ManagedThreadId:{5},IsBackground:{6},IsThreadPoolThread:{7}",
+                              $"第{i}個", TestAsyncLocal.Instance.AsyncLocalString, TestAsyncLocal.Instance.AsyncLocalString, Thread.CurrentThread.CurrentCulture,
+                              Thread.CurrentThread.CurrentUICulture, Thread.CurrentThread.ManagedThreadId, Thread.CurrentThread.IsBackground, Thread.CurrentThread.IsThreadPoolThread);
+                 });
+            }
+            //Parallel.For(1, 16, (index) =>
+            //{
+            //    TestAsyncLocal.Instance.SetAsyncLocalString($"第{index}個");
+            //    TestAsyncLocal.Instance.NormalString = $"第{index}個";
+            //    Console.WriteLine("   Expected '{0}', got '{1}', ThreadLocal value is '{2}',CurrentCulture:{3},CurrentUICulture:{4},ManagedThreadId:{5},IsBackground:{6},IsThreadPoolThread:{7}",
+            //             $"第{index}個", TestAsyncLocal.Instance.AsyncLocalString, TestAsyncLocal.Instance.NormalString, Thread.CurrentThread.CurrentCulture,
+            //             Thread.CurrentThread.CurrentUICulture, Thread.CurrentThread.ManagedThreadId, Thread.CurrentThread.IsBackground, Thread.CurrentThread.IsThreadPoolThread);
+
+            //});
         }
     }
 }
---
+++ test/AsyncLocal.Test/TestAsyncLocal.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace AsyncLocal.Test
+{
+    public class TestAsyncLocal
+    {
+        public static TestAsyncLocal Instance = new TestAsyncLocal();
+        private AsyncLocal<string> _asyncLocalString = new AsyncLocal<string>();
+
+        public string NormalString { get; set; }
+
+        private TestAsyncLocal() { }
+
+
+        public string AsyncLocalString => _asyncLocalString.Value;
+
+
+        public void SetAsyncLocalString(string value)
+        {
+            _asyncLocalString.Value = value;
+        }
+    }
+}
