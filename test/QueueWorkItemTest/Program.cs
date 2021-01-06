ee09f4db630a5ae53450dd09fd036fbdca0fe7d7
CardAppH006 <CardAppH006@CARDAPPH006-PC>
2021-01-06 21:03:10 +0800

    TimerTest

--- test/QueueWorkItemTest/Program.cs
+++ test/QueueWorkItemTest/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace QueueWorkItemTest
 {
@@ -7,9 +9,35 @@ namespace QueueWorkItemTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("start...");
-            ThreadPool.QueueUserWorkItem(Work,1);
-            Thread.Sleep(20000);
+            //Console.WriteLine("start...");
+            //ThreadPool.QueueUserWorkItem(Work, 1);
+            //Thread.Sleep(20000);
+            Task[] tasks = new Task[10];
+            for (int i = 0; i < 10; i++)
+            {
+                var task = Task.Run(() =>
+                {
+                    var item = i;// new Random().Next(1100);
+                    Console.WriteLine("i={1},Thread.CurrentThread.ManagedThreadId:{0},Thread.CurrentThread.IsBackground:{2}, Thread.CurrentThread.IsThreadPoolThread:{3}, Thread.CurrentThread.Name:{4}", Thread.CurrentThread.ManagedThreadId, item, Thread.CurrentThread.IsBackground, Thread.CurrentThread.IsThreadPoolThread, Thread.CurrentThread.Name);
+                });
+                tasks[i] = task;
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                var task = Task.Factory.StartNew(() =>
+                {
+                    var item = i;// new Random().Next(1100);
+                    Console.WriteLine("i={1},Thread.CurrentThread.ManagedThreadId:{0},Thread.CurrentThread.IsBackground:{2}, Thread.CurrentThread.IsThreadPoolThread:{3}, Thread.CurrentThread.Name:{4}", Thread.CurrentThread.ManagedThreadId, item, Thread.CurrentThread.IsBackground, Thread.CurrentThread.IsThreadPoolThread, Thread.CurrentThread.Name);
+                });
+                tasks[i] = task;
+            }
+            //Task.WaitAll(tasks);
+            var whenAllTask= Task.WhenAll(tasks);
+            whenAllTask.Wait();
+           
+          
+
         }
 
         private static void Work(object state)
---
+++ test/TimerTest/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Threading;
+
+namespace TimerTest
+{
+    class Program
+    {
+        private readonly static AutoResetEvent autoResetEvent = new AutoResetEvent(false);
+        private static int _count = 0;
+        static void Main(string[] args)
+        {
+            var timer = new Timer((a)=> {
+                _count++;
+                Console.WriteLine(_count);
+                if (_count > 10) autoResetEvent.Set();
+            },null, Timeout.Infinite, Timeout.Infinite);
+            Console.WriteLine("Hello World!");
+            timer.Change(100, 200);
+           Console.WriteLine(autoResetEvent.WaitOne(2000));
+        }
+    }
+}
